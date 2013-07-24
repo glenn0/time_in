69ee94082c27c7606b061b4e29a28bc2e8c41e42
glenn <glenn@G-Dog.local>
2013-07-25 00:37:21 +0200

    Renamed methods for clarity in TimesController.

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -5,12 +5,12 @@ class TimesController < ApplicationController
 
   def create
     session[:location] = params[:searchTextField]
-    do_geocode(session[:location])
-    do_timelookup(session[:lat], session[:long])
+    geocode_location_to_latlong(session[:location])
+    lookup_time_for_location(session[:lat], session[:long])
     redirect_to root_path
   end
 
-  def do_geocode(location)
+  def geocode_location_to_latlong(location)
     response = Net::HTTP.get_response(URI.parse("http://maps.googleapis.com/maps/api/geocode/json?address=#{Rack::Utils.escape(location)}&sensor=false"))
     json = ActiveSupport::JSON.decode(response.body)
     session[:lat], session[:long] = json["results"][0]["geometry"]["location"]["lat"], json["results"][0]["geometry"]["location"]["lng"]
@@ -18,7 +18,7 @@ class TimesController < ApplicationController
     false # For now, fail silently...
   end
 
-  def do_timelookup(lat, long)
+  def lookup_time_for_location(lat, long)
     response = Net::HTTP.get_response(URI.parse("http://ws.geonames.org/timezoneJSON?lat=#{Rack::Utils.escape(lat)}&lng=#{Rack::Utils.escape(long)}&style=full"))    
     json = ActiveSupport::JSON.decode(response.body)
     session[:time] = json["time"]
